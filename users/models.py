abc34be4c0313cb1e11a19c087e2616ccc2c8b41
김규빈 <inkblot626@gmail.com>
2024-09-23 01:35:58 +0900

    add options for user model

--- users/models.py
+++ users/models.py
@@ -2,7 +2,22 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+
+    class GenderChoices(models.TextChoices):
+        FEMALE = ("female", "Female")
+        MALE = ("male", "Male")
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        GBP = "£"
+
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
+    avatar = models.ImageField(blank=True)
     name = models.CharField(max_length=150, default="")
     is_host = models.BooleanField(default=False)
+    gender = models.CharField(max_length=10, choices=GenderChoices.choices)
+    language = models.CharField(max_length=2, choices=LanguageChoices.choices)
+    currency = models.CharField(max_length=5, choices=CurrencyChoices.choices)
