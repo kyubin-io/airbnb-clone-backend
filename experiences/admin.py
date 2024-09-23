4fa1c329817f5819a199231a8820b06d31c334fb
김규빈 <inkblot626@gmail.com>
2024-09-23 22:16:25 +0900

    create categories

--- experiences/admin.py
+++ experiences/admin.py
@@ -12,9 +12,11 @@ class ExperienceAdmin(admin.ModelAdmin):
         "created_at",
     )
 
+    list_filter = ("category",)
+
 @admin.register(Perk)
 class PerkAdmin(admin.ModelAdmin):
-    
+
     list_display = (
         "name",
         "details",
