fd6f921a02255fc5009594a5156c03746bb01ca3
김규빈 <inkblot626@gmail.com>
2024-09-24 02:15:15 +0900

    add related name

--- direct_messages/models.py
+++ direct_messages/models.py
@@ -7,6 +7,7 @@ class ChattingRoom(CommonModel):
 
     users = models.ManyToManyField(
         "users.User",
+        related_name="chatting_rooms",
     )
 
     def __str__(self):
@@ -22,10 +23,12 @@ class Message(CommonModel):
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
+        related_name="chatting_rooms",
     )
     room = models.ForeignKey(
         "direct_messages.ChattingRoom",
         on_delete=models.CASCADE,
+        related_name="chatting_rooms",
     )
     
     def __str__(self):
