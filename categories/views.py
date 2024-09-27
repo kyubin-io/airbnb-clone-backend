aac3972493c82e172f5c918f8afaf7fbfb0aa645
김규빈 <inkblot626@gmail.com>
2024-09-28 04:31:41 +0900

    ModelViewSet

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class Categories(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
